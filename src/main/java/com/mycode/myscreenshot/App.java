58a35094a3ece4c0d8146905e6f7c122fd271cab
DOIF0568 <DOIF0568@AFF160.atena.local>
2015-06-26 13:20:17 +0900

    first commit

---
+++ src/main/java/com/mycode/myscreenshot/App.java
@@ -0,0 +1,69 @@
+package com.mycode.myscreenshot;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.imageio.ImageIO;
+import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.main.Main;
+
+public class App {
+
+    static int length = 0;
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.addRouteBuilder(new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                from("timer:foo?repeatCount=1").process(new Processor() {
+
+                    @Override
+                    public void process(Exchange exchng) throws Exception {
+                        System.out.println("起動が完了しました。\n終了する時は×ボタンかCtrl+Cを押してください。");
+                    }
+                });
+                from("timer:foo?period=1s").choice().when(new Predicate() {
+
+                    @Override
+                    public boolean matches(Exchange exchange) {
+                        try {
+                            Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+                            BufferedImage bi = (BufferedImage) (Image) clip.getData(DataFlavor.imageFlavor);
+                            byte[] imageInByte;
+                            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                                ImageIO.write(bi, "png", baos);
+                                baos.flush();
+                                imageInByte = baos.toByteArray();
+                            }
+
+                            if (length != imageInByte.length) {
+                                length = imageInByte.length;
+                                String format = new SimpleDateFormat("MMdd_HHmmss").format(new Date()) + ".png";
+                                exchange.getIn().setHeader(Exchange.FILE_NAME, format);
+                                exchange.getIn().setBody(imageInByte);
+                                System.out.println("保存しました。 " + format);
+                                return true;
+                            }
+                        } catch (UnsupportedFlavorException | IOException | NullPointerException e) {
+
+                        }
+                        return false;
+                    }
+                }).to("file:images");
+            }
+        });
+        main.run();
+    }
+}
---
+++ スクリーンショットくん起動.bat
@@ -0,0 +1,1 @@
+java -jar target/MyScreenShot-1.0-SNAPSHOT-jar-with-dependencies.jar
